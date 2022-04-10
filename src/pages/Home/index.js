ae95c73e5fe93a4ff6b2f75fb46e1828b567fdad
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-10 20:18:13 -0300

    feat: add modal base

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Container } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Container, Modal } from '@mui/material'
 
 import { Header } from 'components/Header'
 import { Feed } from 'components/Feed'
 
 export function Home() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [open, setOpen] = useState(false)
+  const handleClose = () => {
+    setOpen(false)
+    navigate(-1)
+  }
+
+  useEffect(() => {
+    if (location.pathname.includes('/userProfile')) setOpen(true)
+  }, [location])
+
   return (
     <>
       <Header />
       <Container maxWidth="sm">
         <Feed />
       </Container>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <h1>hello</h1>
+      </Modal>
     </>
   )
 }
