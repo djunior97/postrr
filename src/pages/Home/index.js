c50350e2bc55aea5dcf41d2b5e8d4c18825e6630
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-10 13:12:47 -0300

    feat: create home page file

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+import { Header } from "components/Header";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="md">
+
+      </Container>
+    </>
+  );
+}
