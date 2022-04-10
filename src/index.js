bda871e1ed598b5a15a08ccd5137941195434431
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-10 12:39:01 -0300

    feat: add custom theme and css base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+
 import { App } from "./App";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
