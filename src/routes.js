b84d283e2982c0ab4e9906aade83ad4bc542c058
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-10 22:59:09 -0300

    feat: add route redirection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 
 import { Home } from 'pages/Home'
 
 export function AppRoutes() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/all')
+    }
+  }, [location, navigate])
+
   return (
     <Routes>
       <Route path="/">
