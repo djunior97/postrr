0d4a79ce0186d16c12cd3f5419bba5fe3f09db3e
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-11 14:59:57 -0300

    feat: add all / following posts toggle

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Divider from '@mui/material/Divider'
-
 import ToggleButton from '@mui/material/ToggleButton'
 
 import { PostBox } from 'components/PostBox'
 import { Post } from 'components/Post'
+
+import { SelectPosts, SelectFollowingPosts } from 'store/posts'
 
 import { FeedContainer, ToggleContainer } from './styles'
 
 export function Feed() {
   const navigate = useNavigate()
   const { filter } = useParams()
+  const allPosts = useSelector(SelectPosts)
+  const followingPosts = useSelector(SelectFollowingPosts)
+  const [posts, setPosts] = useState(allPosts)
   const [alignment, setAlignment] = useState(
     ['all', 'following'].includes(filter) ? filter : 'all',
   )
@@ -22,6 +27,14 @@ export function Feed() {
       navigate(`/${newAlignment}`)
     }
   }
+
+  useEffect(() => {
+    if (alignment === 'all') {
+      setPosts(allPosts)
+    } else {
+      setPosts(followingPosts)
+    }
+  }, [alignment, allPosts, followingPosts])
 
   return (
     <FeedContainer>
@@ -39,20 +52,12 @@ export function Feed() {
         <ToggleButton value="following">Following</ToggleButton>
       </ToggleContainer>
 
-      <Post isQuotePost />
-      <Divider />
-
-      <Post isRepost />
-      <Divider />
-
-      <Post />
-      <Divider />
-
-      <Post isRepost />
-      <Divider />
-
-      <Post />
-      <Divider />
+      {posts.map((post) => (
+        <>
+          <Post key={post.id} post={post} />
+          <Divider />
+        </>
+      ))}
     </FeedContainer>
   )
 }
