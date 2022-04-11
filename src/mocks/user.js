4a38f3d57c15c17a93d4ac23066ed9397411a157
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-11 00:14:36 -0300

    chore: add user and users mocks

---
+++ src/mocks/user.js
@@ -0,0 +1,13 @@
+export const user = {
+  id: 1,
+  name: 'Michael Scott',
+  username: 'best_boss',
+  following: [3, 11, 5, 8, 10],
+  followers: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  picture:
+    'https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/MichaelScott.png',
+  cover:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+  bio: 'World&#39;s Best Boss | Regional Manager @DunderMifflinScranton',
+  joinedDate: 'January 2020',
+}
---
+++ src/mocks/users.js
@@ -0,0 +1,132 @@
+export const users = [
+  {
+    id: 2,
+    name: 'Dwight K. Shrute',
+    username: 'best_boss',
+    following: [],
+    followers: [],
+    picture:
+      'https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/dwight.jpeg',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Beet Farmer | Assistant Regional Manager @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 3,
+    name: 'Jim Halpert',
+    username: 'jhalpert',
+    following: [],
+    followers: [],
+    picture:
+      'https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/jim.gif',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Father of two | Happily married',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 4,
+    name: 'Toby Flanderson',
+    username: 'tobyflanderson',
+    following: [],
+    followers: [],
+    picture:
+      'https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/toby.jpg',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Human Resources @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 5,
+    name: 'Ryan Howard',
+    username: 'howardryan',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/e/e0/Character_-_RyanHoward.PNG/revision/latest/top-crop/width/200/height/150?cb=20200414174545',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Temp @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 6,
+    name: 'Angela Martin',
+    username: 'angelamartin',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/0/0b/Angela_Martin.jpg/revision/latest/top-crop/width/200/height/150?cb=20170701090232',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Accounting @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 7,
+    name: 'Meredith Palmer',
+    username: 'meredith',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/9/90/2009Meredith.jpg/revision/latest/scale-to-width-down/700?cb=20170701084945',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Supplier @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 8,
+    name: 'Andy Bernard',
+    username: 'thenarddog',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/2/20/C0164512-C4C2-485C-9CB6-85B31F642090.jpeg/revision/latest/scale-to-width-down/700?cb=20200103213101',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Salesman @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 9,
+    name: 'Kelly Kapoor',
+    username: 'kapoor',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/6/69/Kelly_Kapoor.jpg/revision/latest/scale-to-width-down/700?cb=20170701090501',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Customer Service @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 10,
+    name: 'Erin Hannon',
+    username: 'erinhannon',
+    following: [],
+    followers: [],
+    picture:
+      'https://static.wikia.nocookie.net/theoffice/images/0/0a/ErinHannon.jpg/revision/latest/scale-to-width-down/666?cb=20170805002853',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Receptionist @DunderMifflinScranton',
+    joinedDate: 'January 2020',
+  },
+  {
+    id: 11,
+    name: 'Pam Beesly Halpert',
+    username: 'pbhalpert',
+    following: [],
+    followers: [],
+    picture:
+      'https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/pam.jpg',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dunder_Mifflin%2C_Inc.svg/1920px-Dunder_Mifflin%2C_Inc.svg.png',
+    bio: 'Mother of two | Happily married',
+    joinedDate: 'January 2020',
+  },
+]
