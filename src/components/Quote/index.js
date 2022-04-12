916d3ab7f23069602899b2e0dd4761d6bf704547
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-12 15:09:56 -0300

    feat: add quote post modal

---
+++ src/components/Quote/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { PostBox } from 'components/PostBox'
+import {
+  QuoteContainer,
+  QuotePostContainer,
+  QuoteUserWrapper,
+  QuotePostContent,
+  ProfilePicture,
+  UserInfo,
+  Name,
+  UserName,
+} from './styles'
+
+export function Quote({ quotePostInfo, handleCloseQuoteModal }) {
+  return (
+    <QuoteContainer>
+      <PostBox
+        isQuotePost
+        quotePostInfo={quotePostInfo}
+        handleCloseQuoteModal={handleCloseQuoteModal}
+      />
+
+      <QuotePostContainer>
+        <QuoteUserWrapper>
+          <ProfilePicture
+            alt="profile picture"
+            src={quotePostInfo.userPicture}
+          />
+          <UserInfo>
+            <Name>{quotePostInfo.name}</Name>
+            <UserName>@{quotePostInfo.username}</UserName>
+          </UserInfo>
+        </QuoteUserWrapper>
+
+        <QuotePostContent>{quotePostInfo.content}</QuotePostContent>
+      </QuotePostContainer>
+    </QuoteContainer>
+  )
+}
+
+Quote.propTypes = {
+  quotePostInfo: PropTypes.shape({
+    post_id: PropTypes.number,
+    content: PropTypes.string,
+    user_id: PropTypes.number,
+    userPicture: PropTypes.string,
+    name: PropTypes.string,
+    username: PropTypes.string,
+  }),
+  handleCloseQuoteModal: PropTypes.func,
+}
---
+++ src/components/Quote/styles.js
@@ -0,0 +1,61 @@
+import styled from '@emotion/styled'
+import { Paper, Avatar } from '@mui/material'
+
+export const QuoteContainer = styled(Paper)`
+  width: 600px;
+  margin: 50px auto;
+  padding: 12px;
+`
+
+export const QuotePostContainer = styled.div`
+  position: relative;
+  width: 100%;
+  border: 1px solid rgb(207, 217, 222);
+  border-radius: 16px;
+  padding: 12px;
+  margin: 0 auto;
+  .MuiAvatar-root {
+    height: 20px;
+    width: 20px;
+    margin-right: 8px;
+  }
+`
+
+export const QuoteUserWrapper = styled.div`
+  position: relative;
+  display: flex;
+  column-gap: 0;
+`
+export const QuotePostContent = styled.p`
+  margin: 0;
+`
+
+export const ProfilePicture = styled(Avatar)`
+  width: 48px;
+  height: 48px;
+`
+export const UserInfo = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  column-gap: 5px;
+
+  p {
+    margin: 0;
+  }
+`
+
+export const Name = styled.p`
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 15px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+export const UserName = styled.p`
+  cursor: pointer;
+  font-size: 14px;
+`
