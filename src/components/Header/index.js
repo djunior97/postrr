10dc22d0651bfce649944d92300fb8126a7b2f74
Dirceu Antunes Junior <dirceujr@Dirceus-MacBook-Pro.local>
2022-04-11 22:27:45 -0300

    feat: add functionality to open user profile from profile picture

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Typography,
-  Container,
-  Avatar,
-} from '@mui/material'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { AppBar, Box, Toolbar, Typography, Container } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+
+import { SelectUserInfo } from 'store/userInfo'
+
+import { ProfilePicture } from './styles'
 
 export function Header() {
   const theme = useTheme()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const loggedUser = useSelector(SelectUserInfo)
 
   return (
     <Box sx={{ flexGrow: 1, marginBottom: '16px' }}>
@@ -29,9 +31,12 @@ export function Header() {
             >
               posterr
             </Typography>
-            <Avatar
+            <ProfilePicture
               alt="Michael Scott"
               src="https://www.bu.edu/lernet/artemis/years/2017/projects/StudentWebsites/Dara/images/MichaelScott.png"
+              onClick={() =>
+                navigate(`${location.pathname}/userProfile/${loggedUser.id}`)
+              }
             />
           </Toolbar>
         </Container>
---
+++ src/components/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+import { Avatar } from '@mui/material'
+
+export const ProfilePicture = styled(Avatar)`
+  cursor: pointer;
+`
